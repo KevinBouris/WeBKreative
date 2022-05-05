ef84f1a6d8a1ef3e6af42ca8954471144be7480e
Kévin Bourisville <web.kreative.34@gmail.com>
2022-05-05 23:16:56 +0200

    mobile nav ok, begin of desktop css

--- webkreative/assets/controllers/hamburger_controller.js
+++ webkreative/assets/controllers/hamburger_controller.js
@@ -2,6 +2,10 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     click() {
-        this.element.classList.toggle('open')
+        this.element.classList.toggle('open');
+        
+        let navList = document.getElementById('nav_list');
+        navList.classList.toggle('slide');
     }
+
 }
