0f0514856d589d4b2503e6c892a0956ea67ebe25
Kévin Bourisville <web.kreative.34@gmail.com>
2022-05-16 12:12:58 +0200

    install easyadmin for backoffice

--- webkreative/src/Controller/HomepageController.php
+++ webkreative/src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/accueil', name: 'homepage')]
-    public function index(): Response
+    public function index(UserRepository $user): Response
     {
+        dd($user->findAll());
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
