74b5791ea1b48bc959825333cc3fda6c9b82f17c
Kévin Bourisville <web.kreative.34@gmail.com>
2022-05-16 12:02:54 +0200

    create user entity

---
+++ webkreative/src/Entity/User.php
@@ -0,0 +1,306 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private $email;
+
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+
+    #[ORM\Column(type: 'string')]
+    private $password;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Firstname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $lastname;
+
+    #[ORM\Column(type: 'date')]
+    private $birth;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $callNumber;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $yearExperience;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $adress;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $sirenNumber;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $OpeningDayContact;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $startHour;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $endHour;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $numberCommit;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $finishProject;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $createdAt;
+
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->Firstname;
+    }
+
+    public function setFirstname(string $Firstname): self
+    {
+        $this->Firstname = $Firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getBirth(): ?\DateTimeInterface
+    {
+        return $this->birth;
+    }
+
+    public function setBirth(\DateTimeInterface $birth): self
+    {
+        $this->birth = $birth;
+
+        return $this;
+    }
+
+    public function getCallNumber(): ?string
+    {
+        return $this->callNumber;
+    }
+
+    public function setCallNumber(?string $callNumber): self
+    {
+        $this->callNumber = $callNumber;
+
+        return $this;
+    }
+
+    public function getYearExperience(): ?int
+    {
+        return $this->yearExperience;
+    }
+
+    public function setYearExperience(?int $yearExperience): self
+    {
+        $this->yearExperience = $yearExperience;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(?string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getSirenNumber(): ?string
+    {
+        return $this->sirenNumber;
+    }
+
+    public function setSirenNumber(?string $sirenNumber): self
+    {
+        $this->sirenNumber = $sirenNumber;
+
+        return $this;
+    }
+
+    public function getOpeningDayContact(): ?\DateTimeInterface
+    {
+        return $this->OpeningDayContact;
+    }
+
+    public function setOpeningDayContact(?\DateTimeInterface $OpeningDayContact): self
+    {
+        $this->OpeningDayContact = $OpeningDayContact;
+
+        return $this;
+    }
+
+    public function getStartHour(): ?\DateTimeInterface
+    {
+        return $this->startHour;
+    }
+
+    public function setStartHour(?\DateTimeInterface $startHour): self
+    {
+        $this->startHour = $startHour;
+
+        return $this;
+    }
+
+    public function getEndHour(): ?\DateTimeInterface
+    {
+        return $this->endHour;
+    }
+
+    public function setEndHour(?\DateTimeInterface $endHour): self
+    {
+        $this->endHour = $endHour;
+
+        return $this;
+    }
+
+    public function getNumberCommit(): ?int
+    {
+        return $this->numberCommit;
+    }
+
+    public function setNumberCommit(?int $numberCommit): self
+    {
+        $this->numberCommit = $numberCommit;
+
+        return $this;
+    }
+
+    public function getFinishProject(): ?int
+    {
+        return $this->finishProject;
+    }
+
+    public function setFinishProject(?int $finishProject): self
+    {
+        $this->finishProject = $finishProject;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
