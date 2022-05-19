ac7f2aa4fda184fcc342404c44bad053c65cb7f9
Kévin Bourisville <web.kreative.34@gmail.com>
2022-05-19 14:51:43 +0200

    get projects in controller and make dynamic display in template

--- webkreative/src/Controller/HomepageController.php
+++ webkreative/src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/accueil', name: 'homepage')]
-    public function index(UserRepository $user): Response
+    public function index(
+        ProjectRepository $projectRepository,
+        UserRepository $user
+    ): Response
     {
         $kebou = $user->findOneByEmail('web.kreative.34@gmail.com');
+        $projects = $projectRepository->findAll();
 
         return $this->render('homepage/index.html.twig', [
             'kebouInfo' => $kebou,
+            'projects' => $projects,
         ]);
     }
 }
