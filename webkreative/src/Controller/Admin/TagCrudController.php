5dab9c480fcc710b1b37764bb78fedac591c04fa
Kévin Bourisville <web.kreative.34@gmail.com>
2022-05-20 16:16:55 +0200

    create custom crud controller for admin

--- webkreative/src/Controller/Admin/TagCrudController.php
+++ webkreative/src/Controller/Admin/TagCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Tag;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class TagCrudController extends AbstractCrudController
 {
@@ -12,14 +16,17 @@ class TagCrudController extends AbstractCrudController
         return Tag::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('name', 'Title'),
+            TextField::new('css_class')->onlyOnForms(),
+            DateTimeField::new('created_at')->onlyOnIndex(),
+            DateTimeField::new('updated_at')->onlyOnIndex(),
+            AssociationField::new('projects')
+                ->setFormTypeOption('choice_label', 'name')
+                ->setFormTypeOption('by_reference', false),
         ];
     }
-    */
 }
