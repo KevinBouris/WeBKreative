b4bd7440667dea322c4ac9ed75eaf75c04180669
Kévin Bourisville <web.kreative.34@gmail.com>
2022-05-20 11:07:23 +0200

    add id field on admin index

--- webkreative/src/Controller/Admin/ProjectCrudController.php
+++ webkreative/src/Controller/Admin/ProjectCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Project;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -20,6 +21,7 @@ class ProjectCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            IdField::new('id')->onlyOnIndex(),
             TextField::new('name'),
             TextField::new('client_name'),
             BooleanField::new('dev_status'),
